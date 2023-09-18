f48eb1db436334582351b23d3de01fbc18dac891
keitanamazue <keitanamazue@gmail.com>
2023-09-18 23:05:15 +0900

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import registerNNPushToken from "native-notify";
 
 export default function App() {
+  registerNNPushToken(12256, "YOY5dSWVwh4qr1ApyBzlH5");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+# expo_notification_49.0
